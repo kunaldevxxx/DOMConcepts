ecc5ad10104743e6c8fb47e329a06af0c2f65ef2
Kunal Khare <khareji594@gmail.com>
2023-08-03 22:23:49 +0530

    Done Work

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var bulb = document.querySelector(".bulb")
 var btn = document.querySelector("button")
+var flag = 0 
 btn.addEventListener("click",function(){
+    if(flag==0){
     bulb.style.backgroundColor="yellow"
+flag=1
+btn.innerHTML="Off"}
+else{
+    bulb.style.backgroundColor="#fff"
+    btn.innerHTML="On"
+    flag=0
+}
+
 })
